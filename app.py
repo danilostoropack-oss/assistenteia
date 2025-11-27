d2bfdc28a1906a02da3e6f9763c94aa6940f6e5f
Danilo <danilo.storopack@gmail.com>
2025-11-26 22:38:15 -0300

    Melhorar exibicao de links YouTube - apenas botao de abertura

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ HTML = """
     <meta charset="utf-8">
     <title>Assistente Tecnico Storopack</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='75' font-size='75' fill='%23005aa9'>S</text></svg>" type="image/svg+xml">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
